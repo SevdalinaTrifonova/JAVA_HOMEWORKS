390563de61988cd1278ebf8efe7acbf70c7deaa6
SevdalinaTrifonova <sevdalina.trifonova@gmail.com>
2021-11-21 23:48:01 +0200

    Homework Lesson 41

--- src/main/java/qa/course/Lecture41.java
+++ src/main/java/qa/course/Lecture41.java
@@ -1,8 +1,6 @@
 package qa.course;
 
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
 
 public class Lecture41 {
 
@@ -11,12 +9,15 @@ public class Lecture41 {
         findBiggestNumber();
         System.out.println("Press any key to continue!");
         scn.nextLine();
+
         calcRectangleArea();
         System.out.println("Press any key to continue!");
         scn.nextLine();
+
         oddNumber();
         System.out.println("Press any key to continue!");
         scn.nextLine();
+
         System.out.println("Run test dayOfWeek()!");
         dayOfWeek(1);
         dayOfWeek(2);
@@ -29,7 +30,7 @@ public class Lecture41 {
         dayOfWeek(0);
     }
 
-    public static void findBiggestNumber(){
+    public static void findBiggestNumber() {
         //print the biggest of tree numbers
 
         Scanner scn = new Scanner(System.in);
@@ -41,21 +42,18 @@ public class Lecture41 {
         System.out.println("Input the third number");
         int c = scn.nextInt();
         int biggestNumber;
-        if (a>=b) {
-            if (a>=c) {
-                biggestNumber=a;
-            }
-            else {
-                biggestNumber=c;
+        if (a >= b) {
+            if (a >= c) {
+                biggestNumber = a;
+            } else {
+                biggestNumber = c;
             }
 
-        }
-        else {
-            if (b>=c) {
-                biggestNumber=b;
-            }
-            else {
-                biggestNumber=c;
+        } else {
+            if (b >= c) {
+                biggestNumber = b;
+            } else {
+                biggestNumber = c;
             }
         }
         System.out.printf("The biggest number is %d!%n", biggestNumber);
@@ -70,10 +68,9 @@ public class Lecture41 {
         float a = scn.nextFloat();
         System.out.println("Input the width");
         float b = scn.nextFloat();
-        if (a<=0 || b<=0) {
+        if (a <= 0 || b <= 0) {
             System.out.printf("It is not possible to build a rectangle with height=%.2f and width=%.2f!%n", a, b);
-        }
-        else {
+        } else {
             System.out.printf("Perimeter of the rectangle is 2*( %.2f + %.2f ) = %.2f%n", a, b, 2 * (a + b));
             System.out.printf("Area of the rectangle is %.2f*%.2f = %.2f%n", a, b, a * b);
         }
@@ -86,27 +83,26 @@ public class Lecture41 {
         System.out.println("Even or odd number?");
         System.out.println("Input number");
         int a = scn.nextInt();
-        System.out.printf("The number %d is "+(a%2==0?"even":"odd") +" !%n", a);
+        System.out.printf("%d is " + (a % 2 == 0 ? "even" : "odd") + " number!%n", a);
     }
 
-    public static void dayOfWeek( int dayNumber) {
+    public static void dayOfWeek(int dayNumber) {
         //prints day of week
 
-        Scanner scn = new Scanner(System.in);
-        String dayOfWeek="";
-        String abr="th";
+        String dayOfWeek = "";
+        String abr = "th";
         switch (dayNumber) {
             case 1:
                 dayOfWeek = "Monday";
-                abr="st";
+                abr = "st";
                 break;
-           case 2:
+            case 2:
                 dayOfWeek = "Tuesday";
-                abr="nd";
+                abr = "nd";
                 break;
             case 3:
                 dayOfWeek = "Wednesday";
-                abr="rd";
+                abr = "rd";
                 break;
             case 4:
                 dayOfWeek = "Thursday";
@@ -120,12 +116,13 @@ public class Lecture41 {
             case 7:
                 dayOfWeek = "Sunday";
                 break;
+            default:
+                break;
         }
 
-        if (dayOfWeek.length()==0) {
-            System.out.printf("Your input is %d. This is an invalid input!%n",dayNumber);
-        }
-        else{
+        if (dayOfWeek.length() == 0) {
+            System.out.printf("Your input is %d. This is an invalid input!%n", dayNumber);
+        } else {
             System.out.printf("The %d%s day of the week is %s%n", dayNumber, abr, dayOfWeek);
         }
 
